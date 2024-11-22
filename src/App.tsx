aa6bd275abbf41b83f1c7b4e94ed8d3fb3d1f726
suin <suinyeze@gmail.com>
2024-11-22 19:46:53 +0900

    いいねボタンの実装場所を用意した
    App.tsxにLikeButtonコンポーネントを追加し、「いいねボタン予定地」というテキストを表示するようにした。
    
    動作確認:
    - ブラウザで http://localhost:5173 にアクセス
    - 「いいねボタン予定地」というテキストが表示されることを確認

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>TypeScriptはいいぞ</h1>
+      <LikeButton />
     </>
   );
+}
+
+function LikeButton() {
+  return <span>いいねボタン予定地</span>;
 }
 
 export default App;
